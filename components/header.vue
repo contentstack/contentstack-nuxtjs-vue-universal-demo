eccc3290a29ef5f8489676605e7fbdc44d64e3a7
rohit mishra <rohit.mishra@raweng.com>
2018-06-29 11:52:29 +0530

    made changes in header

--- components/header.vue
+++ components/header.vue
@@ -6,7 +6,7 @@
         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
           <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
         </button>
-        <a class="navbar-brand" href="#page-top">{{headers.title | exist }}</a>
+        <a class="navbar-brand" href="/">{{headers.title | exist }}</a>
       </div>
 
       <!-- Collect the nav links, forms, and other content for toggling -->
