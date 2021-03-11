d1fc35c8f9ed6c51a5e5f7bf489adaf4b3a35657
Amitkanswal <amit.kanswal@contentstack.com>
2021-03-11 11:27:25 +0530

    resolve bugs and broken link

--- plugins/entry.js
+++ plugins/entry.js
@@ -1,41 +1,36 @@
 /* eslint-disable prefer-promise-reject-errors */
 const contentstack = require("contentstack");
+const Stack = contentstack.Stack(
+  process.env.api_key,
+  process.env.delivery_token,
+  process.env.environment,
+  process.env.region ? process.env.region : "us"
+);
+
+if (process.env.custom_host) {
+  Stack.setHost(process.env.custom_host);
+}
 
 export default {
   getEntry(contentTypeUid) {
     const data = new Promise((resolve, reject) => {
-      if (
-        process.env.api_key &&
-        process.env.delivery_token &&
-        process.env.environment
-      ) {
-        // initializing contentstck sdk
-        const Stack = contentstack.Stack({
-          api_key: process.env.api_key,
-          access_token: process.env.delivery_token,
-          environment: process.env.environment,
-        });
-        // Query
-        Stack.ContentType(contentTypeUid)
-          .Query()
-          .includeReference(["header", "footer"])
-          .toJSON()
-          .find()
-          .then(
-            function success(result) {
-              if (result) {
-                resolve(result[0]);
-              } else {
-                return reject("Internal Error");
-              }
-            },
-            function error(error) {
-              return reject(error);
+      Stack.ContentType(contentTypeUid)
+        .Query()
+        .includeReference(["header", "footer"])
+        .toJSON()
+        .find()
+        .then(
+          function success(result) {
+            if (result) {
+              resolve(result[0]);
+            } else {
+              return reject("Internal Error");
             }
-          );
-      } else {
-        return reject("Please provide valid config parameters");
-      }
+          },
+          function error(error) {
+            return reject(error);
+          }
+        );
     });
     return data;
   },
